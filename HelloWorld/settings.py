7e4e87e47cbba326acbbc1cc871cd29bf505ef18
sandeep <schourey1988@gmail.com>
2015-04-10 18:22:52 +0530

    add the new apps pool ans add the admin section

--- HelloWorld/settings.py
+++ HelloWorld/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'HelloWorldApp',
+    'polls',
 )
 
 MIDDLEWARE_CLASSES = (
--- HelloWorldApp/views.py
+++ HelloWorldApp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import HttpResponse
-
+import datetime
 # Create your views here.
-
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
 def foo(request):
 	return HttpResponse("Hello Python!")
+def demo(request):
+	return HttpResponse("testing Python!")
