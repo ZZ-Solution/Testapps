abadf885c4097a9b4df5053d880d205c7d218d61
sandeep <schourey1988@gmail.com>
2015-04-13 13:00:51 +0530

    add root path and show qustions

--- HelloWorld/urls.py
+++ HelloWorld/urls.py
@@ -1,11 +1,7 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
-from HelloWorldApp.views import foo, demo 
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'HelloWorld.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'HelloWorldApp/$', demo),
-    url(r'^admin/', include(admin.site.urls)), )
+urlpatterns = [
+    url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+]
--- HelloWorldApp/views.py
+++ HelloWorldApp/views.py
@@ -3,7 +3,3 @@ import datetime
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
-def foo(request):
-	return HttpResponse("Hello Python!")
-def demo(request):
-	return HttpResponse("testing Python!")
